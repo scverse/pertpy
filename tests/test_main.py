a9a85780b8f4fb7865d61b2864a67f1b5f638e74
zethson <lukas.heumos@posteo.net>
2021-07-19 17:23:38 +0200

    add pypi-latest & architecture
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 import pytest
 from click.testing import CliRunner
 
-from pertpy import __main__
+from pertpy.__main__ import pertpy_cli
 
 
 @pytest.fixture
@@ -13,5 +13,5 @@ def runner() -> CliRunner:
 
 def test_main_succeeds(runner: CliRunner) -> None:
     """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+    result = runner.invoke(pertpy_cli)
     assert result.exit_code == 0
