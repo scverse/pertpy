fe4bf0e8669cde62272566218e99b3844eeb9237
Koncopd <koncopd@gmail.com>
2022-03-09 12:09:40 +0100

    return transformer

--- pertpy/tools/_kernel_pca.py
+++ pertpy/tools/_kernel_pca.py
@@ -1,11 +1,18 @@
 from sklearn.decomposition import KernelPCA
 
 
-def kernel_pca(adata, n_comps=None, kernel="linear", copy=False, **kwargs):
+def kernel_pca(adata, n_comps=None, kernel="linear", copy=False, return_transformer=False, **kwargs):
     if copy:
         adata = adata.copy()
     transformer = KernelPCA(n_components=n_comps, kernel=kernel, **kwargs)
     X_kpca = transformer.fit_transform(adata.X)
     adata.obsm["X_kpca"] = X_kpca
+
     if copy:
-        return adata
+        if return_transformer:
+            return adata, transformer
+        else:
+            return adata
+    else:
+        if return_transformer:
+            return transformer
