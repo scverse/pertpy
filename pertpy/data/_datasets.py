9d61bd28418d11a477d6e584f5973e8adf00ae4e
antschum <32771191+antschum@users.noreply.github.com>
2022-03-22 14:51:42 +0100

    add bhattacher, sc_sim, sciplex dataloaders

--- pertpy/data/_datasets.py
+++ pertpy/data/_datasets.py
@@ -55,3 +55,99 @@ def burczynski_crispr() -> MuData:
         mudata = mu.read(output_file_path)
 
     return mudata
+
+
+def sc_sim_augur() -> AnnData:
+    """Simulated test dataset used the Usage example for the Augur.
+
+    https://github.com/neurorestore/Augur
+
+    Args:
+        file_path: Path to the dataset
+
+    Returns:
+        :class:`~anndata.AnnData` object of a simulated single-cell RNA seq dataset
+    """
+    output_file_name = "sc_sim_augur.h5ad"
+    output_file_path = settings.datasetdir.__str__() + "/" + output_file_name
+    if not Path(output_file_path).exists():
+        _download(
+            url="https://figshare.com/ndownloader/files/17114054",
+            output_file_name=output_file_name,
+            output_path=settings.datasetdir,
+            is_zip=False,
+        )
+        adata = sc.read_h5ad(filename=settings.datasetdir.__str__() + "/" + output_file_name)
+    else:
+        adata = sc.read_h5ad(output_file_path)
+
+    return adata
+
+
+def bhattacherjee_rna() -> AnnData:
+    """Single-cell data PFC adult mice under cocaine self-administration.
+
+    Adult mice were subject to cocaine self-administration, samples were
+    collected a three time points: Maintance, 48h after cocaine withdrawal and
+    15 days after cocaine withdrawal.
+
+    Args:
+        file_path: Path to the dataset
+
+    Reference:
+        Bhattacherjee A, Djekidel MN, Chen R, Chen W, Tuesta LM, Zhang Y. Cell
+        type-specific transcriptional programs in mouse prefrontal cortex during
+        adolescence and addiction. Nat Commun. 2019 Sep 13;10(1):4169.
+        doi: 10.1038/s41467-019-12054-3. PMID: 31519873; PMCID: PMC6744514.
+
+    Returns:
+        :class:`~anndata.AnnData` object of a single-cell RNA seq dataset
+    """
+    output_file_name = "bhattacherjee_rna.h5ad"
+    output_file_path = settings.datasetdir.__str__() + "/" + output_file_name
+    if not Path(output_file_path).exists():
+        _download(
+            url="https://figshare.com/ndownloader/files/19397624",
+            output_file_name=output_file_name,
+            output_path=settings.datasetdir,
+            is_zip=False,
+        )
+        adata = sc.read_h5ad(filename=settings.datasetdir.__str__() + "/" + output_file_name)
+    else:
+        adata = sc.read_h5ad(output_file_path)
+
+    return adata
+
+
+# 19122572
+def sciplex3() -> AnnData:
+    """SciPlex3 perturbation dataset curated for perturbation modeling.
+
+
+    Args:
+        file_path: Path to the dataset
+
+    Reference:
+        Srivatsan SR, McFaline-Figueroa JL, Ramani V, Saunders L, Cao J, Packer J,
+        Pliner HA, Jackson DL, Daza RM, Christiansen L, Zhang F, Steemers F,
+        Shendure J, Trapnell C. Massively multiplex chemical transcriptomics at
+        single-cell resolution. Science. 2020 Jan 3;367(6473):45-51.
+        doi: 10.1126/science.aax6234. Epub 2019 Dec 5. PMID: 31806696; PMCID: PMC7289078.
+
+    Returns:
+        :class:`~anndata.AnnData` object of a single-cell RNA seq dataset
+    """
+    output_file_name = "sciplex3.h5ad"
+    output_file_path = settings.datasetdir.__str__() + "/" + output_file_name
+    if not Path(output_file_path).exists():
+        _download(
+            url="https://figshare.com/ndownloader/files/19122572",
+            output_file_name=output_file_name,
+            output_path=settings.datasetdir,
+            is_zip=False,
+        )
+        adata = sc.read_h5ad(filename=settings.datasetdir.__str__() + "/" + output_file_name)
+    else:
+        adata = sc.read_h5ad(output_file_path)
+
+    return adata
