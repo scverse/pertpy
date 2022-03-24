ce5f451acd7161f6f1c31459d683863af5710745
zethson <lukas.heumos@posteo.net>
2022-03-24 09:58:58 +0100

    fix isort
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- pertpy/data/__init__.py
+++ pertpy/data/__init__.py
@@ -1,11 +1,11 @@
 from pertpy.data._datasets import (
     burczynski_crispr,
     burczynski_crohn,
+    dixit_2016,
+    dixit_2016_raw,
     frangieh_2021,
     frangieh_2021_raw,
-    dixit_2016_raw,
-    dixit_2016,
-    sciplex3_raw,
-    norman_2019_raw,
     norman_2019,
+    norman_2019_raw,
+    sciplex3_raw,
 )
