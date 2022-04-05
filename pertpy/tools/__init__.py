f108f0b2023254f466cecdd23921a4f027fdba5a
Koncopd <koncopd@gmail.com>
2022-04-05 16:46:20 +0200

    adapt and rm nb

--- pertpy/tools/__init__.py
+++ pertpy/tools/__init__.py
@@ -1,4 +1,3 @@
-from pertpy.tools._example import add_me
+from pertpy.tools._augurpy import Augurpy
 from pertpy.tools._kernel_pca import kernel_pca
 from pertpy.tools._pert_sign import pert_sign
-from pertpy.tools._augurpy import Augurpy
