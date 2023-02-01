a0fe044b9a1b6d4f8cc768fe03583250b5d2f87e
Amir Ali Moinfar <amirali.moinfar@helmholtz-muenchen.de>
2023-02-01 14:18:53 +0100

    Import ordering

--- pertpy/plot/__init__.py
+++ pertpy/plot/__init__.py
@@ -1,5 +1,5 @@
 from pertpy.plot._augurpy import AugurpyPlot as ag  # noqa: N813
-from pertpy.plot._guide_rna import GuideRnaPlot as guide  # noqa: N813
 from pertpy.plot._coda import CodaPlot as coda  # noqa: N813
+from pertpy.plot._guide_rna import GuideRnaPlot as guide  # noqa: N813
 from pertpy.plot._milopy import MilopyPlot as milo  # noqa: N813
 from pertpy.plot._mixscape import MixscapePlot as ms  # noqa: N813
