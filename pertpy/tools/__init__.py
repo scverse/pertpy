418b1ce329a8bc6de6403d52a6455d85d28f7206
zethson <lukas.heumos@posteo.net>
2023-02-22 13:46:55 +0100

    Fix ete3 installation to optional
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- pertpy/tools/__init__.py
+++ pertpy/tools/__init__.py
@@ -1,8 +1,14 @@
+from rich import print
+
 from pertpy.tools._augurpy import Augurpy
 from pertpy.tools._dialogue import Dialogue
 from pertpy.tools._differential_gene_expression import DifferentialGeneExpression
 from pertpy.tools._kernel_pca import kernel_pca
 from pertpy.tools._milopy import Milopy
 from pertpy.tools._mixscape import Mixscape
-from pertpy.tools._sccoda import Sccoda
-from pertpy.tools._tasccoda import Tasccoda
+
+try:
+    from pertpy.tools._sccoda import Sccoda
+    from pertpy.tools._tasccoda import Tasccoda
+except ImportError:
+    print("[bold yellow]To use sccoda or tasccoda please install ete3")
