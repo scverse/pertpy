d70788dc99f7356753978dd4fd8501c38ef068c2
IsraMekki0 <isra.mekki@helmholtz-muenchen.de>
2023-04-06 06:57:21 +0200

    Deleted jaxscgen from plot

--- pertpy/plot/__init__.py
+++ pertpy/plot/__init__.py
@@ -1,4 +1,3 @@
 from pertpy.plot._augurpy import AugurpyPlot as ag  # noqa: N813
-from pertpy.plot._jax_scgen import JaxscgenPlot as scg  # noqa: N813
 from pertpy.plot._milopy import MilopyPlot as milo  # noqa: N813
 from pertpy.plot._mixscape import MixscapePlot as ms  # noqa: N813
