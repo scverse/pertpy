2a678530922bcf1049efb895be0de442ec50b895
zethson <lukas.heumos@posteo.net>
2023-05-06 15:55:49 +0200

    Poetry update
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/tools/test_jax_scgen.py
+++ tests/tools/test_jax_scgen.py
@@ -26,7 +26,7 @@ def test_scgen():
     # reg mean and reg var
     ctrl_adata = adata[((adata.obs["labels"] == "label_0") & (adata.obs["batch"] == "batch_0"))]
     stim_adata = adata[((adata.obs["labels"] == "label_0") & (adata.obs["batch"] == "batch_1"))]
-    eval_adata = ctrl_adata.concatenate(stim_adata, pred, batch_key="concat_batches")
+    eval_adata = ctrl_adata.concat(stim_adata, pred, batch_key="concat_batches")
     label_0 = adata[adata.obs["labels"] == "label_0"]
     sc.tl.rank_genes_groups(label_0, groupby="batch", method="wilcoxon")
     diff_genes = label_0.uns["rank_genes_groups"]["names"]["batch_1"]
