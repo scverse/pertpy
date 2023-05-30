ef6b58ec4414178d112f91052359b5ca79111d69
zethson <lukas.heumos@posteo.net>
2023-05-30 11:29:36 +0200

    update scvi-tools
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- pertpy/__init__.py
+++ pertpy/__init__.py
@@ -4,11 +4,15 @@ __author__ = "Lukas Heumos"
 __email__ = "lukas.heumos@posteo.net"
 __version__ = "0.5.0"
 
-
+from numba import NumbaDeprecationWarning
 from pypi_latest import PypiLatest
 
 pertpy_pypi_latest = PypiLatest("pertpy", __version__)
 pertpy_pypi_latest.check_latest()
+
+import warnings
+
+warnings.filterwarnings("ignore", category=NumbaDeprecationWarning)
 
 from . import data as dt
 from . import plot as pl
