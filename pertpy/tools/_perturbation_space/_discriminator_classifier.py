7293c1f9799b888543beb99589ff6ef0445ed356
zethson <lukas.heumos@posteo.net>
2023-07-20 11:58:08 +0200

    Unpin igraph
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- pertpy/tools/_perturbation_space/_discriminator_classifier.py
+++ pertpy/tools/_perturbation_space/_discriminator_classifier.py
@@ -14,10 +14,11 @@ from pertpy.tools._perturbation_space._perturbation_space import PerturbationSpa
 
 
 class DiscriminatorClassifierSpace(PerturbationSpace):
-    """Leveraging discriminator classifier: The idea here is that we fit either a regressor model for gene expression.
-    here https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7289078/ (Dose-response analysis) and Sup 17-19)
-    and we use either coefficient of the model for each perturbation as a feature or train a classifier example
-    (simple MLP or logistic regression and take the penultimate layer as feature space and apply pseudobulking approach)
+    """Leveraging discriminator classifier. Fit a regressor model to the data and take the feature space.
+
+    See here https://www.ncbi.nlm.nih.gov/pmc/articles/PMC7289078/ (Dose-response analysis) and Sup 17-19)
+    We use either the coefficients of the model for each perturbation as a feature or train a classifier example
+    (simple MLP or logistic regression and take the penultimate layer as feature space and apply pseudobulking approach).
     """
 
     def load(  # type: ignore
