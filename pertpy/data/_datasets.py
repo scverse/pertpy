baf67ff0dc7a8aaeb95347ae0be376574fb31c1d
XinmingTu <tuxm@uw.edu>
2023-07-24 09:44:32 -0700

    fixed the output_file_name of frangieh_2021_raw (#321)
    * add n_jobs support for pairwise distance
    
    * update the n_jobs default to 1
    
    * pre-commit
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * fix output file name
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    Co-authored-by: Tu <tux7@sc1ns001is07.cm.cluster>
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    Co-authored-by: XinmingTu <tux7@gene.com>

--- pertpy/data/_datasets.py
+++ pertpy/data/_datasets.py
@@ -212,7 +212,7 @@ def frangieh_2021_raw() -> AnnData:  # pragma: no cover
     Returns:
         :class:`~anndata.AnnData` object of raw Perturb-CITE-seq data
     """
-    output_file_name = "frangieh_2021.h5ad"
+    output_file_name = "frangieh_2021_raw.h5ad"
     output_file_path = settings.datasetdir.__str__() + "/" + output_file_name
     if not Path(output_file_path).exists():
         _download(
