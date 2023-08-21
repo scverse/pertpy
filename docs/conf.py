123cd0ff8a37dc03d0785b192191097fa1e238db
zethson <lukas.heumos@posteo.net>
2023-08-21 13:13:39 +0200

    Add new wfs
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- docs/conf.py
+++ docs/conf.py
@@ -124,8 +124,10 @@ html_theme_options = {
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-# html_css_files = ["css/override.css", "css/sphinx_gallery.css"]
+html_css_files = ["css/override.css", "css/sphinx_gallery.css"]
 html_show_sphinx = False
+
+sphinx_gallery_conf = {"nested_sections=": False}
 
 
 nbsphinx_prolog = r"""
