72f22fd7309303f99b61532310a233017918d0e1
Tessa Green <tessa_green@g.harvard.edu>
2023-08-23 12:54:37 -0400

    draft plotting functions
    Note: pair plot currently not working

---
+++ pertpy/plot/_dialogue.py
@@ -0,0 +1,75 @@
+import os
+from typing import Literal, Optional, Union
+
+import pandas as pd 
+import scanpy as sc
+import seaborn as sns
+from anndata import AnnData
+import matplotlib.pyplot as plt
+
+class DialoguePlot:
+    @staticmethod
+    def split_violins(
+        adata: AnnData,
+        split_key: str, 
+        split_which: (str,str),
+        celltype_key = str,
+        mcp: str = "mcp_0",
+    ) -> plt.Axes:
+        """
+        Usage: Plots split violin plots for a given MCP and split variable. Any cells 
+        with a value for split_key not in split_which are removed from the plot.
+
+        Args:
+            adata (AnnData): Annotated data object.
+            split_key (str): Variable in adata.obs used to split the data.
+            split_which (str,str): Which values of split_key to plot.
+            celltype_key (str): Key for cell type annotations.
+            mcp (str, optional): Key for MCP data. Defaults to "mcp_0".
+
+        Returns:
+            A :class:`~matplotlib.axes.Axes` object"""
+        
+        # Get a DataFrame with necessary columns
+        df = sc.get.obs_df(adata, [celltype_key, mcp, split_key])
+        df = df[df[split_key].isin(split_which)]
+        df[split_key] = df[split_key].cat.remove_unused_categories()
+
+        
+        
+        # Create split violin plot using Seaborn
+        p2 = sns.violinplot(data=df, x=celltype_key, y=mcp, hue=split_key, split=True)
+
+        #rotate labels for readability
+        p2.set_xticklabels(p2.get_xticklabels(), rotation=90)
+        
+        # Return the matplotlib Axes object
+        return p2
+    def pairplot(
+        adata: AnnData,
+        celltype_key: str,
+        color: str,
+        sample_id: str,
+        mcp: str = "mcp_0"
+    ) -> plt.Axes:
+        """Generate a pairplot visualization for multi-cell perturbation (MCP) data.
+        This function computes the mean of a specified MCP feature (mcp) for each combination of
+        sample and cell type, then creates a pairplot to visualize the relationships between these
+        mean MCP values.
+        
+        Usage: pairplot(adata, celltype_key="Cluster", color="Efficacy", mcp="mcp_0", sample_key="Sample"))
+
+        Args:
+            adata: AnnData object
+            celltype_key: the key in adata.obs for cell type annotations.
+            color: the key in adata.obs for color annotations. This parameter is used as the hue
+            sample_id: the key in adata.obs for the sample annotations.
+            mcp: the key in adata.obs for MCP feature values. Default is "mcp_0".
+        Returns:
+            A :class:`~matplotlib.axes.Axes` object"""    
+        mean_mcps = adata.obs.groupby([sample_id,celltype_key])[mcp].mean()
+        mean_mcps = mean_mcps.reset_index()
+        mcp_pivot = pd.pivot(mean_mcps[[sample_id,celltype_key,mcp]],index=sample_id,columns = celltype_key )[mcp]
+        mcp_pivot = pd.concat([mcp_pivot,adata.obs.groupby([sample_id]).agg(pd.Series.mode)[color]],axis=1)
+        sns.pairplot(mcp_pivot, corner=True, hue=color)
+
