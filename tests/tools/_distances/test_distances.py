35fd863c5e9757e173fcdf40d89a0d60778b4408
zethson <lukas.heumos@posteo.net>
2023-09-06 11:45:40 +0200

    Conditional subsampling
    Signed-off-by: zethson <lukas.heumos@posteo.net>

--- tests/tools/_distances/test_distances.py
+++ tests/tools/_distances/test_distances.py
@@ -18,22 +18,32 @@ pseudo_distances = ["mean_pairwise", "mmd", "r2_distance", "kl_divergence", "t_t
 
 class TestDistances:
     @fixture
-    def adata(self):
+    def adata(self, request):
+        no_subsample_distances = ["kl_divergence", "t_test"]
+        distance = request.node.callspec.params["distance"]
+
         adata = pt.dt.distance_example()
-        adata_subsampled = sc.pp.subsample(adata, 0.1, copy=True)
-        return adata_subsampled
+        if distance not in no_subsample_distances:
+            adata = sc.pp.subsample(adata, 0.001, copy=True)
+        else:
+            adata = sc.pp.subsample(adata, 0.1, copy=True)
+
+        return adata
 
     @mark.parametrize("distance", actual_distances)
     def test_distance_axioms(self, adata, distance):
         # Test if distances are well-defined in accordance with metric axioms
         Distance = pt.tl.Distance(distance, "X_pca")
         df = Distance.pairwise(adata, groupby="perturbation", verbose=True)
-        # (M1) Positiv definiteness
+
+        # (M1) Positive definiteness
         assert all(np.diag(df.values) == 0)  # distance to self is 0
         assert len(df) == np.sum(df.values == 0)  # distance to other is not 0
+
         # (M2) Symmetry
         assert np.sum(df.values - df.values.T) == 0
         assert df.columns.equals(df.index)
+
         # (M3) Triangle inequality (we just probe this for a few random triplets)
         for _i in range(10):
             triplet = np.random.choice(df.index, size=3, replace=False)
@@ -43,6 +53,7 @@ class TestDistances:
     def test_distance(self, adata, distance):
         Distance = pt.tl.Distance(distance, "X_pca")
         df = Distance.pairwise(adata, groupby="perturbation", verbose=True)
+
         assert isinstance(df, DataFrame)
         assert df.columns.equals(df.index)
         assert np.sum(df.values - df.values.T) == 0  # symmetry
