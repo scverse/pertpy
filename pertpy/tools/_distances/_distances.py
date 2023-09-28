e012c09f2d0a644512e41ca559c4e5905b4ab733
eljas <eljas.roellin@ikmail.com>
2023-09-28 11:24:42 +0200

    wasserstein float return value

--- pertpy/tools/_distances/_distances.py
+++ pertpy/tools/_distances/_distances.py
@@ -543,7 +543,7 @@ class WassersteinDistance(AbstractDistance):
         solver = Sinkhorn()
         # Solve OT problem
         ot = solver(ot_prob, **kwargs)
-        return ot.reg_ot_cost
+        return ot.reg_ot_cost.item()
 
 
 class EuclideanDistance(AbstractDistance):
