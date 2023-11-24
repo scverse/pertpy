467f3a19864a20bd9eb9bb4ac82bd02b4a621580
Xichen Wu <102925032+wxicu@users.noreply.github.com>
2023-11-24 20:19:12 +0100

    Update pertpy/tools/_metadata/_cell_line.py
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>

--- pertpy/tools/_metadata/_cell_line.py
+++ pertpy/tools/_metadata/_cell_line.py
@@ -409,7 +409,7 @@ class CellLineMetaData:
         if len(not_matched_identifiers) > 0:
             if isinstance(show, str):
                 if show != "all":
-                    raise ValueError("Only a non-positive value or all is accepted.")
+                    raise ValueError("Only a non-negative value or all is accepted.")
                 else:
                     show = len(not_matched_identifiers)
 
