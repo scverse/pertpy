9b715b8331d0a6502769da608af22dbf0d671637
Xichen Wu <102925032+wxicu@users.noreply.github.com>
2023-12-20 21:42:09 +0100

    Add moa and compound annotation (#444)
    * fix test warning
    
    * add moa and compound
    
    * fix pre commit
    
    * add test and lookup
    
    * fix merge bug
    
    * update doc, fix test
    
    * fix typo + precommit
    
    * Update pertpy/tools/_metadata/_cell_line.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update pertpy/tools/_metadata/_cell_line.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update pertpy/tools/_metadata/_cell_line.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update pertpy/tools/_metadata/_compound.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update pertpy/tools/_metadata/_look_up.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * add helper function
    
    * Update pertpy/tools/_metadata/_cell_line.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update pertpy/tools/_metadata/_look_up.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * Update pertpy/tools/_metadata/_compound.py
    
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    
    * improve docstring, add parent class
    
    * parent class.
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * rename the class
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fix doc
    
    * remove cachedir, use sequence instead of list
    
    * add type checking
    
    * add test scripts, add compare_categories
    
    * Refactor
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * Refactor
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    * add correlate and plot_correlation function, simplify the code
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * format document
    
    * Refactoring and move plot
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    
    ---------
    
    Signed-off-by: zethson <lukas.heumos@posteo.net>
    Co-authored-by: Lukas Heumos <lukas.heumos@posteo.net>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

---
+++ pertpy/metadata/_metadata.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Literal
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+class MetaData:
+    def _warn_unmatch(
+        self,
+        total_identifiers: int,
+        unmatched_identifiers: Sequence[str],
+        query_id: str,
+        reference_id: str,
+        metadata_type: Literal[
+            "cell line",
+            "protein expression",
+            "bulk RNA",
+            "drug response",
+            "moa",
+            "compound",
+        ] = "cell line",
+        verbosity: int | str = 5,
+    ) -> None:
+        """Helper function to print out the unmatched identifiers.
+
+        Args:
+            total_identifiers: The total number of identifiers in the `adata` object.
+            unmatched_identifiers: Unmatched identifiers in the `adata` object.
+            query_id: The column of `.obs` with cell line information.
+            reference_id: The type of cell line identifier in the meta data.
+            metadata_type: The type of metadata where some identifiers are not matched during annotation, cell line, protein expression, bulk RNA expression, drug response, moa or compound. Defaults to "cell line".
+            verbosity: The number of unmatched identifiers to print, can be either non-negative values or "all". Defaults to 5.
+        """
+        if isinstance(verbosity, str):
+            if verbosity != "all":
+                raise ValueError("Only a non-negative value or 'all' is accepted.")
+            else:
+                verbosity = len(unmatched_identifiers)
+
+        if len(unmatched_identifiers) == total_identifiers:
+            hint = ""
+            if metadata_type in ["protein expression", "bulk RNA", "drug response"]:
+                hint = "Additionally, call the `CellLineMetaData.annotate()` function to acquire more possible query IDs that can be used for cell line annotation purposes."
+            raise ValueError(
+                f"Attempting to match the query id {query_id} in 'adata.obs' to the reference id {reference_id} in the metadata.\n"
+                "However, none of the query IDs could be found in the {metadata_type} annotation data.\n"
+                "To resolve this issue, call the `lookup()` function to create a LookUp object.\n"
+                "This enables obtaining the count of matched identifiers in the AnnData object for different types of reference and query IDs.\n"
+                f"{hint}"
+            )
+        if len(unmatched_identifiers) == 0:
+            return
+        if isinstance(verbosity, int) and verbosity >= 0:
+            verbosity = min(verbosity, len(unmatched_identifiers))
+            if verbosity > 0:
+                print(
+                    f"[bold blue]There are {total_identifiers} identifiers in `adata.obs`."
+                    f"However, {len(unmatched_identifiers)} identifiers can't be found in the {metadata_type} annotation,"
+                    "leading to the presence of NA values for their respective metadata.\n",
+                    "Please check again: ",
+                    *unmatched_identifiers[:verbosity],
+                    "...",
+                    sep="\n- ",
+                )
+        else:
+            raise ValueError("Only 'all' or a non-negative value is accepted.")
