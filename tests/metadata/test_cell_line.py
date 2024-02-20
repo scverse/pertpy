ce51aa69d3d2f8713b349c47ccbaa8d05e5477b2
wxicu <xichenwu@outlook.com>
2024-02-20 21:06:19 +0100

    fix pre commit

--- tests/metadata/test_cell_line.py
+++ tests/metadata/test_cell_line.py
@@ -23,19 +23,14 @@ class TestMetaData:
 
         obs = pd.DataFrame(
             {
-                "DepMap_ID": ["ACH-000016", "ACH-000049", "ACH-001208", "ACH-000956"]
-                * NUM_CELLS_PER_ID,
+                "DepMap_ID": ["ACH-000016", "ACH-000049", "ACH-001208", "ACH-000956"] * NUM_CELLS_PER_ID,
                 "perturbation": ["Midostaurin"] * NUM_CELLS_PER_ID * 4,
             },
             index=[str(i) for i in range(NUM_GENES)],
         )
 
         var_data = {"gene_name": [f"gene{i}" for i in range(1, NUM_GENES + 1)]}
-        var = (
-            pd.DataFrame(var_data)
-            .set_index("gene_name", drop=False)
-            .rename_axis("index")
-        )
+        var = pd.DataFrame(var_data).set_index("gene_name", drop=False).rename_axis("index")
 
         X = sparse.csr_matrix(X)
         adata = anndata.AnnData(X=X, obs=obs, var=var)
@@ -44,9 +39,7 @@ class TestMetaData:
 
     def test_cell_line_annotation(self, adata):
         self.pt_metadata.annotate(adata=adata)
-        assert (
-            len(adata.obs.columns) == len(self.pt_metadata.depmap.columns) + 1
-        )  # due to the perturbation column
+        assert len(adata.obs.columns) == len(self.pt_metadata.depmap.columns) + 1  # due to the perturbation column
         stripped_cell_line_name = ["SLR21", "HEKTE", "TK10", "22RV1"] * NUM_CELLS_PER_ID
         assert stripped_cell_line_name == list(adata.obs["StrippedCellLineName"])
 
@@ -57,9 +50,7 @@ class TestMetaData:
 
     def test_protein_expression_annotation(self, adata):
         self.pt_metadata.annotate(adata)
-        self.pt_metadata.annotate_protein_expression(
-            adata, query_id="StrippedCellLineName"
-        )
+        self.pt_metadata.annotate_protein_expression(adata, query_id="StrippedCellLineName")
 
         assert len(adata.obsm) == 1
         assert adata.obsm["proteomics_protein_intensity"].shape == (
@@ -69,9 +60,7 @@ class TestMetaData:
 
     def test_bulk_rna_expression_annotation(self, adata):
         self.pt_metadata.annotate(adata)
-        self.pt_metadata.annotate_bulk_rna(
-            adata, query_id="DepMap_ID", cell_line_source="broad"
-        )
+        self.pt_metadata.annotate_bulk_rna(adata, query_id="DepMap_ID", cell_line_source="broad")
 
         assert len(adata.obsm) == 1
         assert adata.obsm["bulk_rna_broad"].shape == (
