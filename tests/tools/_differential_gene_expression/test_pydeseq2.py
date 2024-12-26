19e753abfc901cbe93e4a80db9f4a8bff277a6c0
Gregor Sturm <mail@gregor-sturm.de>
2024-12-26 13:20:19 +0100

    Update tests

--- tests/tools/_differential_gene_expression/test_pydeseq2.py
+++ tests/tools/_differential_gene_expression/test_pydeseq2.py
@@ -14,6 +14,17 @@ def test_pydeseq2_simple(test_adata):
     res_df = method.test_contrasts(method.contrast("condition", "A", "B"))
 
     assert len(res_df) == test_adata.n_vars
+    # Compare against snapshot
+    npt.assert_almost_equal(
+        res_df.p_value.values,
+        [0.00017, 0.00033, 0.00051, 0.0286, 0.03207, 0.04723, 0.11039, 0.11452, 0.3703, 0.99625],
+        decimal=4,
+    )
+    npt.assert_almost_equal(
+        res_df.log_fc.values,
+        [0.58207, 0.53855, -0.4121, 0.63281, -0.63283, -0.27066, -0.21271, 0.38601, 0.13434, 0.00146],
+        decimal=4,
+    )
 
 
 def test_pydeseq2_complex(test_adata):
@@ -28,6 +39,17 @@ def test_pydeseq2_complex(test_adata):
     assert len(res_df) == test_adata.n_vars
     # Check that the index of the result matches the var_names of the AnnData object
     assert set(test_adata.var_names) == set(res_df["variable"])
+    # Compare against snapshot
+    npt.assert_almost_equal(
+        res_df.p_value.values,
+        [7e-05, 0.00012, 0.00035, 0.01062, 0.01906, 0.03892, 0.10755, 0.11175, 0.36631, 0.94952],
+        decimal=4,
+    )
+    npt.assert_almost_equal(
+        res_df.log_fc.values,
+        [-0.42347, 0.58802, 0.53528, 0.73147, -0.67374, -0.27158, -0.21402, 0.38953, 0.13511, -0.01949],
+        decimal=4,
+    )
 
 
 def test_pydeseq2_formula(test_adata):
@@ -41,7 +63,3 @@ def test_pydeseq2_formula(test_adata):
     res_2 = model2.test_contrasts(model2.contrast("condition", "A", "B"))
 
     npt.assert_almost_equal(res_2.log_fc.values, res_1.log_fc.values)
-
-
-# TODO: there should be a test checking if, for a concrete example, the output p-values and effect sizes are what
-# we expect (-> frozen snapshot, that way we also get a heads-up if something changes upstream)
