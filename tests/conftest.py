b0b57c1ce74a8b86456c5b75fbbe0d66e3d36bf8
Lukas Heumos <lukas.heumos@posteo.net>
2025-05-02 13:41:35 +0200

    Remove pytest time
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,36 +1,5 @@
-import time
-
 import numpy as np
 import pytest
-
-
-class FileTimer:
-    def __init__(self) -> None:
-        self.timers: dict[str, float] = {}
-        self.current_file: str | None = None
-
-
-file_tracker = FileTimer()
-
-
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_protocol(item, nextitem):
-    file_path = str(item.fspath)
-
-    # If this is the first test from this file
-    if file_path != file_tracker.current_file:
-        file_tracker.current_file = file_path
-        if file_path not in file_tracker.timers:
-            file_tracker.timers[file_path] = time.time()
-
-    yield
-
-    # If this is the last test in the file
-    if nextitem is None or str(nextitem.fspath) != file_path:
-        duration = time.time() - file_tracker.timers[file_path]
-        reporter = item.config.pluginmanager.get_plugin("terminalreporter")
-        if reporter and hasattr(reporter, "_tw"):
-            reporter._tw.write(f" [{duration:.4f}s]", cyan=True)
 
 
 def pytest_addoption(parser):
