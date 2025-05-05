118b86dc14b5ed878e5674f8e28aaa603e8d4d29
Lukas Heumos <lukas.heumos@posteo.net>
2025-05-05 13:28:13 -0700

    Remove unused params

--- pertpy/tools/_augur.py
+++ pertpy/tools/_augur.py
@@ -164,8 +164,6 @@ class Augur:
             classifier: classifier to use in calculating the area under the curve.
                         Either random forest classifier or logistic regression for categorical data
                         or random forest regressor for continous data
-            params: parameters used to populate the model object. Default values are `n_estimators` =
-                    100, `max_depth` = None, `max_features` = 2, `penalty` = `l2`, `random_state` = None.
             n_estimators: Number of trees in the forest.
             max_depth: Maximal depth of each tree.
             max_features: Maximal number of features considered when looking at best split.
