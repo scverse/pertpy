b5f62ff9cc7085033bcaa38f604e665b0eae4733
Lukas Heumos <lukas.heumos@posteo.net>
2025-05-23 11:04:13 +0200

    Remove decoupler
    Signed-off-by: Lukas Heumos <lukas.heumos@posteo.net>

--- pertpy/tools/_perturbation_space/_simple.py
+++ pertpy/tools/_perturbation_space/_simple.py
@@ -4,9 +4,8 @@ from typing import TYPE_CHECKING
 
 import matplotlib.pyplot as plt
 import numpy as np
+import scanpy as sc
 from anndata import AnnData
-from decoupler import get_pseudobulk as dc_get_pseudobulk
-from decoupler import plot_psbulk_samples as dc_plot_psbulk_samples
 from sklearn.cluster import DBSCAN, KMeans
 
 from pertpy._doc import _doc_params, doc_common_plot_args
@@ -167,52 +166,11 @@ class PseudobulkSpace(PerturbationSpace):
                 adata = adata_emb
 
         adata.obs[target_col] = adata.obs[target_col].astype("category")
-        ps_adata = dc_get_pseudobulk(adata, sample_col=target_col, layer=layer_key, groups_col=groups_col, **kwargs)  # type: ignore
+        ps_adata = sc.get.aggregate(adata, by=[target_col, groups_col], func="sum", layer=layer_key)
 
         ps_adata.obs[target_col] = ps_adata.obs[target_col].astype("category")
 
         return ps_adata
-
-    @_doc_params(common_plot_args=doc_common_plot_args)
-    def plot_psbulk_samples(  # pragma: no cover # noqa: D417
-        self,
-        adata: AnnData,
-        groupby: str,
-        *,
-        return_fig: bool = False,
-        **kwargs,
-    ) -> Figure | None:
-        """Plot the pseudobulk samples of an AnnData object.
-
-        Plot the count number vs. the number of cells per pseudobulk sample.
-
-        Args:
-            adata: Anndata containing pseudobulk samples.
-            groupby: `.obs` column to color the samples by.
-            {common_plot_args}
-            **kwargs: Are passed to decoupler's plot_psbulk_samples.
-
-        Returns:
-            If `return_fig` is `True`, returns the figure, otherwise `None`.
-
-        Examples:
-            >>> import pertpy as pt
-            >>> adata = pt.dt.zhang_2021()
-            >>> ps = pt.tl.PseudobulkSpace()
-            >>> pdata = ps.compute(
-            ...     adata, target_col="Patient", groups_col="Cluster", mode="sum", min_cells=10, min_counts=1000
-            ... )
-            >>> ps.plot_psbulk_samples(pdata, groupby=["Patient", "Major celltype"], figsize=(12, 4))
-
-        Preview:
-            .. image:: /_static/docstring_previews/pseudobulk_samples.png
-        """
-        fig = dc_plot_psbulk_samples(adata, groupby, return_fig=True, **kwargs)
-
-        if return_fig:
-            return fig
-        plt.show()
-        return None
 
 
 class KMeansSpace(ClusteringSpace):
